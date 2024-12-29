0fe9a937169fd2fa6c809f39fcfa5176b36c0220
Link 1503 <104648877+Link1503@users.noreply.github.com>
2024-12-29 20:08:07 +0100

    Optimierung der "if(Gespielte_Schleife != "nichts" )" schleife; EXPERIMENTELL

--- player.java
+++ player.java
@@ -256,214 +256,193 @@ public class Logic implements IGameHandler {
 	        }
             
            
-            
-            
-         if(Gespielte_Schleife != "nichts" ){
-	        int Nicht_begebare_Felder = 0;
-            Move = 0;
+            if(Gespielte_Schleife != "nichts" ){
+    	        int Nicht_begebare_Felder = 0;
+                Move = 0;
 
-	     	if ( gameState.getBoard().getNextField(Field.HARE, Eigne_Position) != null ) {	
-	     		Nächter_Hase = gameState.getBoard().getNextField(Field.HARE, Eigne_Position); 
-		     	}
-	     	if ( gameState.getBoard().getNextField(Field.HARE, Eigne_Position) == null ) {	
-	     		Nächter_Hase = 65; 
-	     	    }
-	     	
-	     	if ( gameState.getBoard().getNextField(Field.HEDGEHOG, Eigne_Position) != null ) {	
-	     	    Nächter_Igel = gameState.getBoard().getNextField(Field.HEDGEHOG, Eigne_Position); 
-		     	}
-	     	if ( gameState.getBoard().getNextField(Field.HEDGEHOG, Eigne_Position) == null ) {	
-	     		Nächter_Igel = 65; 
-	     	    } 
-	     	if ( gameState.getBoard().getNextField(Field.SALAD, Eigne_Position) != null ) {	
-		     	Nächter_Salat = gameState.getBoard().getNextField(Field.SALAD, Eigne_Position); 
-		     	}
-	     	if ( gameState.getBoard().getNextField(Field.SALAD, Eigne_Position) == null ) {	
-		        Nächter_Salat = 65; 
-	     	    }
+                Nächter_Hase = gameState.getBoard().getNextField(Field.HARE, Eigne_Position) != null ? gameState.getBoard().getNextField(Field.HARE, Eigne_Position) : 65;
+
+                Nächter_Igel = gameState.getBoard().getNextField(Field.HEDGEHOG, Eigne_Position) != null ? gameState.getBoard().getNextField(Field.HEDGEHOG, Eigne_Position) : 65;
+
+                Nächter_Salat = gameState.getBoard().getNextField(Field.SALAD, Eigne_Position) != null ? gameState.getBoard().getNextField(Field.SALAD, Eigne_Position) : 65;
 
 
-            while (Nächter_Igel < Nächstes_Feld  || 
-            	   Nächter_Hase < Nächstes_Feld  && Kartenspielbarkeit == false && Nächstes_Feld != Nächter_Hase || 
-            	   Nächter_Salat < Nächstes_Feld  && Eigne_Salate == 0 && Nächstes_Feld !=  Nächter_Salat && Eigne_Position != Nächter_Salat) {
+                while (Nächter_Igel < Nächstes_Feld ||
+                	       (Nächter_Hase < Nächstes_Feld && !Kartenspielbarkeit && Nächstes_Feld != Nächter_Hase) ||
+                	       (Nächter_Salat < Nächstes_Feld && Eigne_Salate == 0 && Nächstes_Feld != Nächter_Salat && Eigne_Position != Nächter_Salat)) {
 
-                if (Nächter_Igel < Nächstes_Feld ) {
-                	 
-                        Nicht_begebare_Felder = Nicht_begebare_Felder + 1;
-                        
-                        if ( gameState.getBoard().getNextField(Field.HEDGEHOG, Nächter_Igel) != null ) {
-        	        	    Nächter_Igel = gameState.getBoard().getNextField(Field.HEDGEHOG, Nächter_Igel);
-                                  }
-		                if ( gameState.getBoard().getNextField(Field.HEDGEHOG, Nächter_Igel) == null ) {
-			        	    Nächter_Igel = 65;
-            					  }
-                         }
+                	    Nicht_begebare_Felder++;
 
-                if  (Nächter_Hase < Nächstes_Feld  && Kartenspielbarkeit == false && Nächstes_Feld != Nächter_Hase) {
+                	    Integer nächstesFeld = null;
+                	    Field typ = null;
 
-                        Nicht_begebare_Felder = Nicht_begebare_Felder + 1;
+                	    if (Nächter_Igel < Nächstes_Feld) {
+                	        typ = Field.HEDGEHOG;
+                	        nächstesFeld = Nächter_Igel;
+                	    } else if (Nächter_Hase < Nächstes_Feld && !Kartenspielbarkeit && Nächstes_Feld != Nächter_Hase) {
+                	        typ = Field.HARE;
+                	        nächstesFeld = Nächter_Hase;
+                	    } else if (Nächter_Salat < Nächstes_Feld && Eigne_Salate == 0 && Nächstes_Feld != Nächter_Salat && Eigne_Position != Nächter_Salat) {
+                	        typ = Field.SALAD;
+                	        nächstesFeld = Nächter_Salat;
+                	    }
 
-                        if ( gameState.getBoard().getNextField(Field.HARE, Nächter_Hase) != null ) {
-                        	Nächter_Hase = gameState.getBoard().getNextField(Field.HARE, Nächter_Hase);
-                                  }
-		                if (gameState.getBoard().getNextField(Field.HARE, Nächter_Hase) == null ) {
-			        	    Nächter_Hase = 65;
-            					  } 
-                        }
+                	    if (typ != null && nächstesFeld != null) {
+                	        Integer next = gameState.getBoard().getNextField(typ, nächstesFeld);
+                	        if (next != null) {
+                	            switch (typ) {
+                	                case HEDGEHOG: Nächter_Igel = next; break;
+                	                case HARE: Nächter_Hase = next; break;
+                	                case SALAD: Nächter_Salat = next; break;
+                	            }
+                	        } else {
+                	            switch (typ) {
+                	                case HEDGEHOG: Nächter_Igel = 65; break;
+                	                case HARE: Nächter_Hase = 65; break;
+                	                case SALAD: Nächter_Salat = 65; break;
+                	            }
+                	        }
+                	    }
+                	}
                 
-                if (Nächter_Salat < Nächstes_Feld  && Eigne_Salate == 0 && Nächstes_Feld !=  Nächter_Salat && Eigne_Position != Nächter_Salat) {
-
-	                    Nicht_begebare_Felder = Nicht_begebare_Felder + 1;
-	                    
-	                    if ( gameState.getBoard().getNextField(Field.SALAD, Nächter_Salat) != null ) {
-                        	Nächter_Salat = gameState.getBoard().getNextField(Field.SALAD, Nächter_Salat);
-                                  }
-		                if (gameState.getBoard().getNextField(Field.SALAD, Nächter_Salat) == null ) {
-			        	    Nächter_Salat = 65;
-            					  } 
-	     		    
-                        }
-	    
-            }
-            
-            
-                 int Karte = 0;
+                
+                     int Karte = 0;
+                     
+                     if( Nächstes_Feld == Nächter_Hase ) {
+    		     
+    			     if (Gegner_Karten_Typ[1] != Karten_Typ[1] && Karten_Typ[1] != 0 && Gegner_Position > 57 && Gengner_Karroten > Eigne_Karrotten + 100) {
+    			    	
+    			    	 Karte = 1; // Karroten tauschen
+    			    	 
+    			            }
+    			     
+    			     if (Eigne_Salate != 0 && Karten_Typ[0] != 0) {
+    			    	 
+    			    	 Karte = 0; //Saladfress 	 
+    			    	 
+    			            }
+                      }
+                     if( Nächstes_Feld == Nächter_Markt ) {
+            		     
+                	 if (Gegner_Karten_Typ[1] >= Karten_Typ[1] ||  Karten_Typ[1] == 0) {
+        		    	 
+        		    	  Karte = 1;  // Karroten tauschen
+        		            }
+        		      
+        		     if (Gegner_Karten_Typ[1] < Karten_Typ[1] &&  Karten_Typ[1] != 0 && Eigne_Salate != 0) {
+        		    	 
+        		    	  Karte = 0;  //Saladfressen
+        		            }
+                      }
                  
-                 if( Nächstes_Feld == Nächter_Hase ) {
-		     
-			     if (Gegner_Karten_Typ[1] != Karten_Typ[1] && Karten_Typ[1] != 0 && Gegner_Position > 57 && Gengner_Karroten > Eigne_Karrotten + 100) {
-			    	
-			    	 Karte = 1; // Karroten tauschen
-			    	 
-			            }
-			     
-			     if (Eigne_Salate != 0 && Karten_Typ[0] != 0) {
-			    	 
-			    	 Karte = 0; //Saladfress 	 
-			    	 
-			            }
-                  }
-                 if( Nächstes_Feld == Nächter_Markt ) {
-        		     
-            	 if (Gegner_Karten_Typ[1] >= Karten_Typ[1] ||  Karten_Typ[1] == 0) {
-    		    	 
-    		    	  Karte = 1;  // Karroten tauschen
-    		            }
-    		      
-    		     if (Gegner_Karten_Typ[1] < Karten_Typ[1] &&  Karten_Typ[1] != 0 && Eigne_Salate != 0) {
-    		    	 
-    		    	  Karte = 0;  //Saladfressen
-    		            }
-                  }
+                   if (Gegner_Position < Nächstes_Feld && Eigne_Position < Gegner_Position &&  Gegner_FeldTyp != Field.HEDGEHOG && Gegner_FeldTyp != Field.HARE  ) {
+    		    	
+    		    
+                	Nicht_begebare_Felder = Nicht_begebare_Felder + 1;
+    		               }   	
+                
+                Move = Feld_Dist - Nicht_begebare_Felder + Karte - 1;
+                
+                
+    	    
+             }
              
-               if (Gegner_Position < Nächstes_Feld && Eigne_Position < Gegner_Position &&  Gegner_FeldTyp != Field.HEDGEHOG && Gegner_FeldTyp != Field.HARE  ) {
-		    	
-		    
-            	Nicht_begebare_Felder = Nicht_begebare_Felder + 1;
-		               }   	
-            
-            Move = Feld_Dist - Nicht_begebare_Felder + Karte - 1;
-            
-            
-	    
-         }
-         
-	    
-	    
-	    while (Anzahl_Mögliche_Züge <=  Move) {
-	    	
-	    	Move = Anzahl_Mögliche_Züge - 1;
-	    	Gespielte_Schleife = "Notfall Funktion";
-	    }
-	    
-	    if  ( Eigne_Position == 63 && Nächstes_Feld !=  64 && Eigne_Salate == 0 ){ 
-            Move = 2;
-	    }
-	    if  ( Eigne_Position == 63 &&  Eigne_Karrotten <= 10 && Eigne_Salate == 0 ){ 
-            Move = 0;
-	    }
-	  
-	    if (gameState.mustEatSalad(gameState.getCurrentPlayer()) == true) {
-	    	 
-	    	// Für den Fall das wir auf einem Salatfeld sind muss immer "Move = 0" gesetzt werde da es sonst zu einem Fehler kommt 
-	    	
-	    	Gespielte_Schleife = "Salatfressen Funktion ";
-	    	
-		     Move = 0;	
+    	    
+    	    
+    	    while (Anzahl_Mögliche_Züge <=  Move) {
+    	    	
+    	    	Move = Anzahl_Mögliche_Züge - 1;
+    	    	Gespielte_Schleife = "Notfall Funktion";
+    	    }
+    	    
+    	    if  ( Eigne_Position == 63 && Nächstes_Feld !=  64 && Eigne_Salate == 0 ){ 
+                Move = 2;
+    	    }
+    	    if  ( Eigne_Position == 63 &&  Eigne_Karrotten <= 10 && Eigne_Salate == 0 ){ 
+                Move = 0;
+    	    }
+    	  
+    	    if (gameState.mustEatSalad(gameState.getCurrentPlayer()) == true) {
+    	    	 
+    	    	// Für den Fall das wir auf einem Salatfeld sind muss immer "Move = 0" gesetzt werde da es sonst zu einem Fehler kommt 
+    	    	
+    	    	Gespielte_Schleife = "Salatfressen Funktion ";
+    	    	
+    		     Move = 0;	
 
-	    }
-	    
-	  Move move = Mögliche_Züge.get(Move);   // hier wird für die berechnung des Zuges "Move" eingesetzt und dann aus der Liste an möglichen Zügen ausgweählt 
-   	
-   
+    	    }
+    	    
+    	  Move move = Mögliche_Züge.get(Move);   // hier wird für die berechnung des Zuges "Move" eingesetzt und dann aus der Liste an möglichen Zügen ausgweählt 
+       	
        
-    		
-   	// Der folgende Abschnnit dient dazu um in der Konsole zu sehen was durchgeführt wird:
-   
-    /*--------------------------------------------------------------------------------------------------------*/
-   
-   
-    log.info("Sende {} nach {}ms.", move, System.currentTimeMillis() - startTime);
-     
-    System.out.println("Spielrunde: " + gameState.getTurn() + " |  Menge an Karrotten " + Eigne_Karrotten);
-    System.out.println("Nächster Markt: " + Nächter_Markt);
-    System.out.println("Mögliche Züge: " + Mögliche_Züge + "   " + Anzahl_Mögliche_Züge + "\n");
-    System.out.println("Gespielter Zug; " + move + "   " + Move + "\n");
-    System.out.println("Feld: " + Eigne_FeldTyp + " | " + Eigne_Position + "\n"); 
-    System.out.println("Distans zum Markt: " + Nächter_Salat + " | " + "Begebare Felder: " + Dist + "\n");
-    System.out.println("Gespielte Schleife: \u001B[31m" + Gespielte_Schleife + "\u001B[0m");
-    System.out.println( Kartenspielbarkeit);
-    System.out.println("Karten =  " + "Saladfressen:" + Karten_Typ[0] + " | "+ "Karotten tauschen:" + Karten_Typ[1] + " | " + "Zurückfallen:"+ Karten_Typ[2] + " | " + "Vorrücken:" + Karten_Typ[3]);  
-    System.out.println("Karten-Gegner =  " + "Saladfressen:" + Gegner_Karten_Typ[0] + " | "+ "Karotten tauschen:" + Gegner_Karten_Typ[1] + " | " + "Zurückfallen:"+ Gegner_Karten_Typ[2] + " | " + "Vorrücken:" +Gegner_Karten_Typ[3]);
-    System.out.println("________________________________________________________________________" + "\n");
-    
-    
-    /*---------------------------------------------------------------------------------------------------------*/	
-     
-    
-    return move;
-     
-     
-       }
-  
-  
-  
+           
+        		
+       	// Der folgende Abschnnit dient dazu um in der Konsole zu sehen was durchgeführt wird:
+       
+        /*--------------------------------------------------------------------------------------------------------*/
+       
+       
+        log.info("Sende {} nach {}ms.", move, System.currentTimeMillis() - startTime);
+         
+        System.out.println("Spielrunde: " + gameState.getTurn() + " |  Menge an Karrotten " + Eigne_Karrotten);
+        System.out.println("Nächster Markt: " + Nächter_Markt);
+        System.out.println("Mögliche Züge: " + Mögliche_Züge + "   " + Anzahl_Mögliche_Züge + "\n");
+        System.out.println("Gespielter Zug; " + move + "   " + Move + "\n");
+        System.out.println("Feld: " + Eigne_FeldTyp + " | " + Eigne_Position + "\n"); 
+        System.out.println("Distans zum Markt: " + Nächter_Salat + " | " + "Begebare Felder: " + Dist + "\n");
+        System.out.println("Gespielte Schleife: \u001B[31m" + Gespielte_Schleife + "\u001B[0m");
+        System.out.println( Kartenspielbarkeit);
+        System.out.println("Karten =  " + "Saladfressen:" + Karten_Typ[0] + " | "+ "Karotten tauschen:" + Karten_Typ[1] + " | " + "Zurückfallen:"+ Karten_Typ[2] + " | " + "Vorrücken:" + Karten_Typ[3]);  
+        System.out.println("Karten-Gegner =  " + "Saladfressen:" + Gegner_Karten_Typ[0] + " | "+ "Karotten tauschen:" + Gegner_Karten_Typ[1] + " | " + "Zurückfallen:"+ Gegner_Karten_Typ[2] + " | " + "Vorrücken:" +Gegner_Karten_Typ[3]);
+        System.out.println("________________________________________________________________________" + "\n");
+        
+        
+        /*---------------------------------------------------------------------------------------------------------*/	
+         
+        
+        return move;
+         
+         
+           }
+      
+      
+      
 
-  																						
-  
-  @Override
-  
-  
-  
-  public void onUpdate(IGameState gameState) {
-	  
-	  
-    this.gameState = (GameState) gameState;
-    log.info("Zug: {} Dran: {}", gameState.getTurn(), gameState.getCurrentTeam());
-    
-    
-  		}
+      																						
+      
+      @Override
+      
+      
+      
+      public void onUpdate(IGameState gameState) {
+    	  
+    	  
+        this.gameState = (GameState) gameState;
+        log.info("Zug: {} Dran: {}", gameState.getTurn(), gameState.getCurrentTeam());
+        
+        
+      		}
 
-  
-  public void onGameOver(GameResult data) {       //Wird aufgerufen, wenn das Spiel beendet ist.
-	  
-	  
-    log.info("Das Spiel ist beendet, Ergebnis: {}", data);
-    
-  		}
+      
+      public void onGameOver(GameResult data) {       //Wird aufgerufen, wenn das Spiel beendet ist.
+    	  
+    	  
+        log.info("Das Spiel ist beendet, Ergebnis: {}", data);
+        
+      		}
 
-   
-  @Override
-  
-  public void onError(String error) {   		//Wird aufgerufen, wenn der Server einen Fehler meldet. Bedeutet auch den Abbruch des Spiels. 
+       
+      @Override
+      
+      public void onError(String error) {   		//Wird aufgerufen, wenn der Server einen Fehler meldet. Bedeutet auch den Abbruch des Spiels. 
 
-	  
-	  
-    log.warn("Fehler: {}", error);
-    
-    
-  }
-  
-  
-  
-}
+    	  
+    	  
+        log.warn("Fehler: {}", error);
+        
+        
+      }
+      
+      
+      
+    }
